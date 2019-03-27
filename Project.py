4d7219fbd6fe156b69198d8ad63ca424e22fbd1e
jamesnelly <g00346996@gmit.ie>
2019-03-27 09:44:56 +0000

    matching regular ecpressions almost complete

--- Project.py
+++ Project.py
@@ -1,35 +1,33 @@
-
-"""
 def shunt(infix):
+    # Special characters for regular expressions and their precedence
     specials = {'*': 50, '.': 40, '|': 30}
 
-    pofix: str = ""
-    stack: str = ""
+    # Will eventually be the output stack
+    pofix = ""
+    # Operator stack
+    stack = ""
 
     for c in infix:
-        print("c: ", c, " stack: ", stack, "  postfix: ", pofix)
+       # print("c: ", c, " stack: ", stack, "  postfix: ", pofix)
         if c == '(':
             stack = stack + c
         elif c == ')':
-
             while stack[-1] != '(':
                 pofix, stack = pofix + stack[-1], stack[:-1]
             stack = stack[:-1]
         elif c in specials:
             while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
                 pofix, stack = pofix + stack[-1], stack[:-1]
-        stack = stack + c
-    else:
-        pofix = pofix + c
+            stack = stack + c
+        else:
+            pofix = pofix + c
 
-        while stack:
-            pofix, stack = pofix + stack[-1], stack[:-1]
+    while stack:
+        pofix, stack = pofix + stack[-1], stack[:-1]
 
     return pofix
 
-
-print(shunt("a.b+c"))
-"""
+print(shunt("(a.b)|(c*.d)"))
 # Represents a state with two arrows, labelled by a label
 # Use None for a label representing "e" arrows.
 class  state: 
@@ -59,13 +57,12 @@ def compile(pofix):
             newnfa = nfa(nfa1.initial, nfa2.accept)
             nfastack.append(newnfa)
         elif c == '|':
-            # POP two NFA's off the stack.
+             # POP two NFA's off the stack.
              nfa2 = nfastack.pop()
              nfa1 = nfastack.pop()
              # Create a new initial state, connect it to initial states
              # of the two NFA's popped from the stck
              initial = state()
-             accept = state()
              initial.edge1 = nfa1.initial
              initial.edge2 = nfa2.initial
              # Create a new accept state, connecting the accept states
@@ -101,6 +98,63 @@ def compile(pofix):
             # Push new NFA to the stack
             newnfa = nfa(initial, accept)
             nfastack.append(newnfa)
-        return nfastack.pop()
+
+    # nfastack should only have a single nfa on it at this point.    
+    return nfastack.pop()
+
+def followes(state):
+    ''' Return the set of states that can be reached from state following e arrows.'''
+    # Craete a new set, with state as its only member.
+    states = set()
+    states.add(state)
+
+    # check if state has arrows labelled e from it.
+    if state.label is None:
+        # Check if edge1 is a state.
+        if state.edge1 is not None:
+            # If there's an edge1, follow it
+            states |= followes(state.edge1)
+        # Check if edge2 is a state.
+        if state.edge2 is not None:
+            # If there's an edge2, follow it.
+            states |= followes(state.edge2)
+
+    # Return the set of states
+    return states
+
+def match(infix, string):
+    # Shunt and compile the regular expression.
+    postfix = shunt(infix)
+    nfa = compile(postfix)
+
+    # The current set of states and the next set of states.
+    current = set()
+    next = set()
+
+    # Add the initial state to the current set.
+    current |= followes(nfa.initial)
+
+    # Loop through each character in the string.
+    for s in string:
+        # Loop through the current set of states.
+        for c in current:
+            # Check if that state is labelled s.
+            if c.label == s:
+                # Add the edge1 state to the next set.
+                next |= followes(c.edge1)
+        # Set current to next, and clear out next.
+        current = next
+        next = set()
+
+    # Check if the accept state is in the set of current states.
+    return (nfa.accept in current)
+
+# some tests.
+infixes = ["a.b.c*", "a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
+strings = ["", "abc", "abbc", "abcc", "abad", "abbbc"]
+
+for i in infixes:
+    for s in strings:
+        print(match(i, s), i, s)
 # nfastack should only have a single nfa on it at this point.
-print(compile("ab.cd.|"))
+#print(compile("ab.cd.|"))
